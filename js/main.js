57b90d9b06e64b23bdb8578082ac1da93ce3cfcc
TawanAlves <tas.tawan@gmail.com>
2022-11-30 21:30:31 -0300

    criacao de imagem em slide

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.logo').slick({
+
+   infinite: true,
+   speed: 200,
+   slidesToShow: 1,
+   centerMode: true,
+   variableWidth: true,
+   slidesToScroll: 1,
+ autoplay: true,
+ autoplaySpeed: 2000,
+   
+ });
